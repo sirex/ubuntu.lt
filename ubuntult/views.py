20d590b150a8c68983520316094236c9af9a7639
Mantas <sirexas@gmail.com>
2016-06-05 15:31:34 +0300

    Fix parameter validation issue

--- ubuntult/views.py
+++ ubuntult/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponsePermanentRedirect
+from django.http import HttpResponsePermanentRedirect, Http404
 
 from ubuntult.models import PhpBBForumRefs, PhpBBTopicRefs
+from ubuntult.forms import ViewForumParams, ViewTopicParams
 
 
 def phpbb_viewforum(request):
-    ref = get_object_or_404(
-        PhpBBForumRefs,
-        phpbb_forum_id=request.GET.get('f'),
-    )
-    return HttpResponsePermanentRedirect(ref.category.get_absolute_url())
+    form = ViewForumParams(request.GET)
+    if form.is_valid():
+        params = form.cleaned_data
+        ref = get_object_or_404(
+            PhpBBForumRefs,
+            phpbb_forum_id=params['f'],
+        )
+        return HttpResponsePermanentRedirect(ref.category.get_absolute_url())
+    else:
+        raise Http404(form.errors.as_text())
 
 
 def phpbb_viewtopic(request):
-    ref = get_object_or_404(
-        PhpBBTopicRefs,
-        phpbb_forum_id=request.GET.get('f'),
-        phpbb_topic_id=request.GET.get('t'),
-    )
-    return HttpResponsePermanentRedirect(ref.topic.get_absolute_url())
+    form = ViewTopicParams(request.GET)
+    if form.is_valid():
+        params = form.cleaned_data
+        ref = get_object_or_404(
+            PhpBBTopicRefs,
+            phpbb_forum_id=params['f'],
+            phpbb_topic_id=params['t'],
+        )
+        return HttpResponsePermanentRedirect(ref.topic.get_absolute_url())
+    else:
+        raise Http404(form.errors.as_text())
